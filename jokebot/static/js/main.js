22d6bf3eaaf65ff75f52d706caae3c17e72531dc
jon <jon@test.dev>
2020-12-06 13:51:07 -0400

    stub out js functions called during joke telling or initialization

---
+++ jokebot/static/js/main.js
@@ -0,0 +1,36 @@
+
+/*
+ function called when user requests jokebot tell a 
+ joke. fetches a joke from database and begins the 
+ interactive joke-telling process as joke-teller
+*/
+function jbd_tell()
+{
+   // stub
+   return;
+}
+
+
+/*
+ function called when user offers to tell jokebot a 
+ joke. Begins the interactive joke-telling process 
+ as joke-listener, stores joke if complete without 
+ errors
+*/
+function jbd_listen()
+{
+   // stub
+   return;
+}
+
+
+/*
+ function called when user submits text input to jokebot 
+ while either telling or listening to a joke. 
+*/
+function jbd_submit()
+{
+   // stub
+   return;
+}
+
