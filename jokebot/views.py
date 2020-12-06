ac9150395878ac9fe1b76e1444e0522c375874bd
jon <jon@test.dev>
2020-12-06 10:05:02 -0400

    initial load

---
+++ jokebot/views.py
@@ -0,0 +1,6 @@
+from django.http import HttpResponse
+from django.views.generic import TemplateView
+
+# Create your views here.
+class IndexPageView(TemplateView):
+    template_name = 'index.html'
