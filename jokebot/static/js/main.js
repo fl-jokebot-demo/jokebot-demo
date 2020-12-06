5d4e14a76b2004c85b3ad65d6058cebca5ac30a1
jon <jon@test.dev>
2020-12-06 19:24:49 -0400

    update js used to pull Joke model count on initial page load

--- jokebot/static/js/main.js
+++ jokebot/static/js/main.js
@@ -39,10 +39,92 @@ function jbd_submit()
 
 /*
  function called on page load to find joke count
+ and enable the "tell me a joke button if we have
+ any jokes in the database
 */
-function jbd_joke_count()
+function jbd_joke_count( token, elem )
 {
-   // stub
-   return;
-}
 
+   var joke_count_promise = new Promise(function( resolve, reject )
+   {
+      var oXmlHttp;
+
+      try
+      {
+         try
+         {
+            oXmlHttp=new ActiveXObject( "Microsoft.XMLHTTP" );
+         }
+         catch( e )
+         {
+	    try
+            {
+               oXmlHttp = new XMLHttpRequest();
+            }
+            catch( e )
+            {
+               reject( e.description );
+            }
+         }
+
+         oXmlHttp.onreadystatechange=function()
+         {
+            if ( oXmlHttp.readyState == 4 )
+            {
+               if ( oXmlHttp.status == 200 )
+               {
+                  // we're good
+                  resolve( oXmlHttp.response );
+               }
+               else
+               {
+                  reject( oXmlHttp.status.toString() );
+               }
+            }
+         };
+
+         oXmlHttp.open( "POST", "joke_count", true );
+         oXmlHttp.setRequestHeader( "Content-type", "application/x-www-form-urlencoded" );
+         oXmlHttp.send( "csrfmiddlewaretoken=" + token );
+
+      }
+      catch( e )
+      {
+         reject( e.description );
+      }
+
+   });
+
+   /*
+     look for a count object after our
+     async call is done
+   */
+   joke_count_promise.then(function(value)
+   {
+
+      // assume bad till we know good
+      elem.style.display = "none";
+
+      try
+      {
+
+         let j = JSON.parse( value );
+         if ( j.errors != 0 )
+         {
+            return;
+         }
+
+         if ( j.count > 0 )
+         {
+            elem.style.display = "block";
+         }
+
+      }
+      catch( e )
+      {
+         // do something inspired here
+      }
+
+   });
+
+}
